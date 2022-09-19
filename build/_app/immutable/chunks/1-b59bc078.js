e7029764c12c0421c797cb9b3f01392176eb8e5e
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-09-19 16:02:32 +0200

    add static website

---
+++ build/_app/immutable/chunks/1-b59bc078.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-25557627.js";export{t as component};
---
+++ build/_app/immutable/chunks/2-db1c9928.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-0a4f4efa.js";export{t as component};
