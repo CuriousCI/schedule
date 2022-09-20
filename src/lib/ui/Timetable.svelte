3a5a6d41e06a135afeb12c60e9cfeee30527b9ef
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-09-20 15:22:11 +0200

    add channel 2 & fix table display

--- src/lib/ui/Timetable.svelte
+++ src/lib/ui/Timetable.svelte
@@ -1,11 +1,39 @@
 <script lang="ts">
 	import type Class from '$lib/models/Class';
+	import { get } from 'svelte/store';
 	import ClassCard from './ClassCard.svelte';
 	import Tooltip from './Tooltip.svelte';
 
 	export let timetable: Map<string, Class[]>,
 		hours: string[] = ['08:00', '09:00', '10:00', '11:00', '12:00'],
 		days: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+
+	function offsetTime(time: string, offset: number): string {
+		const hour = parseInt(time.slice(0, 2)) + offset;
+		return `${hour < 10 ? '0' : ''}${hour}:00`;
+	}
+
+	function between(time: string, startTime: string, endTime: string): boolean {
+		const hour = parseInt(time.slice(0, 2)),
+			startHour = parseInt(startTime.slice(0, 2)),
+			endHour = parseInt(endTime.slice(0, 2));
+
+		return hour >= startHour && hour <= endHour;
+	}
+
+	let grid: Map<string, boolean> = new Map();
+
+	$: if (timetable) {
+		for (const hour of hours)
+			for (const day of days)
+				for (const _class of timetable.get(day) || [])
+					if (between(hour, _class.from, _class.to)) {
+						grid.set(`${hour}${day}`, true);
+						break;
+					}
+
+		grid = grid;
+	}
 </script>
 
 <table class="h-[80%] w-[80%] table-fixed">
@@ -23,20 +51,24 @@
 		<tbody>
 			{#each hours as hour}
 				<tr>
-					<td>{hour}</td>
+					<td>{hour} - {offsetTime(hour, 1)}</td>
 					{#each days as day}
-						{#each timetable.get(day) || [] as _class}
-							{#if _class.from == hour}
-								<td rowspan={_class.duration} style="background: var(--{_class.subject?.color})">
-									<Tooltip>
-										<span class="text-bg text-xl font-bold">{_class.subject?.id}</span>
-										<div slot="info">
-											<ClassCard {_class} />
-										</div>
-									</Tooltip>
-								</td>
-							{/if}
-						{/each}
+						{#if grid.get(`${hour}${day}`)}
+							{#each timetable.get(day) || [] as _class}
+								{#if _class.from == hour}
+									<td rowspan={_class.duration} style="background: var(--{_class.subject?.color})">
+										<Tooltip>
+											<span class="text-bg text-xl font-bold">{_class.subject?.id}</span>
+											<div slot="info">
+												<ClassCard {_class} />
+											</div>
+										</Tooltip>
+									</td>
+								{/if}
+							{/each}
+						{:else}
+							<td />
+						{/if}
 					{/each}
 				</tr>
 			{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,8 @@
 	let teachers: Map<string, Teacher>,
 		buildings: Map<string, Building>,
 		subjects: Map<string, Subject>,
-		timetable: Map<string, Class[]> = new Map();
+		channel1: Map<string, Class[]> = new Map(),
+		channel2: Map<string, Class[]> = new Map();
 
 	onMount(async () => {
 		teachers = new Map(
@@ -28,12 +29,12 @@
 			(await (await fetch('subjects.json')).json()).map((subject: Subject) => [subject.id, subject])
 		);
 
-		const t: Map<string, { [key: string]: string }[]> = new Map(
+		const t1: Map<string, { [key: string]: string }[]> = new Map(
 			Object.entries(await (await fetch('channel1.json')).json())
 		);
 
-		for (let [day, classes] of t.entries()) {
-			timetable.set(
+		for (let [day, classes] of t1.entries()) {
+			channel1.set(
 				day,
 				classes.map((_class: { [key: string]: string }) => {
 					return {
@@ -48,14 +49,51 @@
 			);
 		}
 
-		timetable = timetable;
+		channel1 = channel1;
+
+		const t2: Map<string, { [key: string]: string }[]> = new Map(
+			Object.entries(await (await fetch('channel2.json')).json())
+		);
+
+		for (let [day, classes] of t2.entries()) {
+			channel2.set(
+				day,
+				classes.map((_class: { [key: string]: string }) => {
+					return {
+						subject: subjects.get(_class['subject']),
+						teacher: teachers.get(_class['teacher']),
+						building: buildings.get(_class['building']),
+						from: _class['from'],
+						to: _class['to'],
+						duration: parseInt(_class['duration'])
+					} as Class;
+				})
+			);
+		}
+
+		channel2 = channel2;
 	});
 </script>
 
 <main class="grid place-items-center h-screen">
-	<input type="text" class="absolute top-4 left-4" />
+	<h1 class="self-end text-xl font-bold">Channel 1</h1>
+	<Timetable timetable={channel1} />
+
+	<h1 class="self-end text-xl font-bold">Channel 2</h1>
 	<Timetable
-		{timetable}
-		hours={['08:00', '09:00', '10:00', '11:00', '12:00', '16:00', '17:00', '18:00']}
+		timetable={channel2}
+		hours={[
+			'08:00',
+			'09:00',
+			'10:00',
+			'11:00',
+			'12:00',
+			'13:00',
+			'14:00',
+			'15:00',
+			'16:00',
+			'17:00',
+			'18:00'
+		]}
 	/>
 </main>
