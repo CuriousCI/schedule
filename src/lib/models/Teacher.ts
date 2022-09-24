b6892286609b02d6cfef4abb28be61af012ee903
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-09-24 19:11:43 +0200

    Add more teacher info

--- src/lib/models/Teacher.ts
+++ src/lib/models/Teacher.ts
@@ -2,7 +2,9 @@ export default interface Teacher {
 	id: string;
 	name: string;
 	surname: string;
+	picture?: string;
 	github?: string;
 	website?: string;
+	profile?: string;
 	email?: string;
 }
--- src/lib/ui/TeacherCard.svelte
+++ src/lib/ui/TeacherCard.svelte
@@ -9,7 +9,20 @@
 		{teacher?.name}
 		{teacher?.surname}
 	</span>
-	<a href={teacher?.github}>GitHub 🐙</a>
-	<a href={teacher?.website}>Sito ⌨️</a>
-	<a href="mailto:{teacher?.website}">Email 📤</a>
+	{#if teacher?.picture}
+		<img src={teacher.picture} alt="{teacher.name} {teacher.surname}" />
+		<br />
+	{/if}
+	{#if teacher?.github}
+		<a href={teacher?.github}>GitHub 🐙</a>
+	{/if}
+	{#if teacher?.profile}
+		<a href={teacher?.profile}>Profilo ️📰</a>
+	{/if}
+	{#if teacher?.email}
+		<a href="mailto:{teacher?.email}">Email 📤</a>
+	{/if}
+	{#if teacher?.website}
+		<a href={teacher?.website}>Sito ⌨️</a>
+	{/if}
 </div>
