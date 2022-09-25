7f3260156d1768ad155fd8c27370e3a5b7da1950
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-09-25 16:44:24 +0200

    try new svelte version

---
+++ docs/_app/immutable/chunks/1-327fde36.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-443e10ef.js";export{t as component};
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,33 +1,65 @@
-const o = "schedule", i = "version_01", c = o + i, r = [
-  "/schedule/",
-  "/schedule/index.html"
-];
-self.addEventListener("fetch", function(e) {
-  console.log("fetch request : " + e.request.url), e.respondWith(
-    caches.match(e.request).then(function(n) {
-      return n ? (console.log("responding with cache : " + e.request.url), n) : (console.log("file is not cached, fetching : " + e.request.url), fetch(e.request));
+const o = {
+  toString: () => {
+    throw new Error("`timestamp` has been removed from $service-worker. Use `version` instead");
+  }
+}, u = [
+  "/schedule/_app/immutable/start-03d8a5fe.js",
+  "/schedule/_app/immutable/components/layout.svelte-ac6a93da.js",
+  "/schedule/_app/immutable/components/error.svelte-443e10ef.js",
+  "/schedule/_app/immutable/components/pages/_page.svelte-3a13c998.js",
+  "/schedule/_app/immutable/assets/_page-b0f78fea.css",
+  "/schedule/_app/immutable/modules/pages/_layout.js-7b9cbfbc.js",
+  "/schedule/_app/immutable/chunks/singletons-41b9e760.js",
+  "/schedule/_app/immutable/chunks/index-d10d3ea9.js",
+  "/schedule/_app/immutable/chunks/_layout-8d2a742b.js",
+  "/schedule/_app/immutable/chunks/0-40490e2d.js",
+  "/schedule/_app/immutable/chunks/1-327fde36.js",
+  "/schedule/_app/immutable/chunks/2-ba00064d.js"
+], d = [
+  "/schedule/.nojekyll",
+  "/schedule/buildings.json",
+  "/schedule/channel1.json",
+  "/schedule/channel2.json",
+  "/schedule/logo_192.png",
+  "/schedule/logo_512.png",
+  "/schedule/logo_72.png",
+  "/schedule/manifest.json",
+  "/schedule/subjects.json",
+  "/schedule/teachers.json"
+], c = self, l = `cache${o}`, h = u.concat(d), r = new Set(h);
+c.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(l).then((s) => s.addAll(h)).then(() => {
+      c.skipWaiting();
     })
   );
 });
-self.addEventListener("install", function(e) {
+c.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.open(c).then(function(n) {
-      return console.log("installing cache : " + c), n.addAll(r);
+    caches.keys().then(async (s) => {
+      for (const t of s)
+        t !== l && await caches.delete(t);
+      c.clients.claim();
     })
   );
 });
-self.addEventListener("activate", function(e) {
-  e.waitUntil(
-    caches.keys().then(function(n) {
-      const l = n.filter(function(t) {
-        return t.indexOf(o);
-      });
-      return l.push(c), Promise.all(
-        n.map(function(t, s) {
-          if (l.indexOf(t) === -1)
-            return console.log("deleting cache : " + n[s]), caches.delete(n[s]);
-        })
-      );
-    })
+async function p(e) {
+  const s = await caches.open(`offline${o}`);
+  try {
+    const t = await fetch(e);
+    return s.put(e, t.clone()), t;
+  } catch (t) {
+    const a = await s.match(e);
+    if (a)
+      return a;
+    throw t;
+  }
+}
+c.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET" || e.request.headers.has("range"))
+    return;
+  const s = new URL(e.request.url), t = s.protocol.startsWith("http"), a = s.hostname === self.location.hostname && s.port !== self.location.port, n = s.host === self.location.host && r.has(s.pathname), i = e.request.cache === "only-if-cached" && !n;
+  t && !a && !i && e.respondWith(
+    (async () => n && await caches.match(e.request) || p(e.request))()
   );
 });
